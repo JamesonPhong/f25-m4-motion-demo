80090e4dd709328bc9d7c17a2ff8976b47a56594
JamesonPhong <000882371@mohawkcollege.ca>
2025-10-03 11:27:52 -0400

    Add a whole lotta things

--- motion-demo-many-eyeballs/Game.cs
+++ motion-demo-many-eyeballs/Game.cs
@@ -5,30 +5,82 @@ using System.Numerics;
 // The namespace your code is in.
 namespace MohawkGame2D
 {
-    /// <summary>
     ///     Your game code goes inside this class!
-    /// </summary>
     public class Game
     {
         // Place your variables here:
+        Vector2[] positions = 
+            [
+                new Vector2(200, 200),
+                new Vector2(50, 50),
+                new Vector2(350, 300),
+                new Vector2(75, 325),
+                new Vector2(275, 40),
+                new Vector2(225, 375),
+            ];
+       
+        int[] radii;
 
-
-        /// <summary>
         ///     Setup runs once before the game loop begins.
-        /// </summary>
         public void Setup()
         {
             Window.SetTitle("Cool Title");
             Window.SetSize(400, 400);
+
+            radii = new int[positions.Length];
+            for (int i = 0; i < radii.Length; i++)
+            {
+                radii[i] = Random.Integer(10, 50);
+            }
         }
 
-        /// <summary>
         ///     Update runs every frame.
-        /// </summary>
         public void Update()
         {
             Window.ClearBackground(Color.OffWhite);
+            
+            for (int i = 0; i < positions.Length; i += 1)
+            {
+                Vector2 position = positions[i];
+                int radius = radii[i];
+                float corneaR = radius;
+                float irisR = radius * 0.7f;
+                float pupilR = radius * 0.35f;
+                DrawEyeball(position, corneaR, irisR, pupilR);
+            }
         }
+        void DrawEyeball(Vector2 eyePosition, float corneaR, float irisR, float pupilR)
+        {
+            // Get both X & Y coords as a vector
+            Vector2 mousePosition = Input.GetMousePosition();
+
+            // To go from A to B, we do B - A
+            Vector2 vectorFromEyeToMouse = mousePosition - eyePosition;
+
+            // Split vector into Direction & Magnitude (Distance)
+            Vector2 direction = Vector2.Normalize(vectorFromEyeToMouse);
+            float distance = vectorFromEyeToMouse.Length();
+
+            // Calculate where Iris (and Pupil) need to go
+            Vector2 irisPosition = eyePosition + direction * (corneaR - irisR);
+
+            Draw.LineSize = 1;
+            Draw.LineColor = Color.Black;
+
+            // Draw Cornea
+            Draw.FillColor = Color.White;
+            Draw.Circle(eyePosition, corneaR);
+
+            // Draw Iris
+            Draw.FillColor = Color.Green;
+            Draw.Circle(irisPosition, irisR);
+
+            // Draw Pupil
+            Draw.FillColor = Color.Black;
+            Draw.Circle(irisPosition, pupilR);
+
+        }
+
     }
 
 }
